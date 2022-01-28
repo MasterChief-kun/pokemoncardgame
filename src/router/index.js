dab8b3a3560059f1d3e4942d6d9c45d83116effb
Rohan Jeendgar <coolbirb2309@gmail.com>
2022-01-28 18:37:07 +0530

    Update Paths

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,17 @@ import { createRouter, createWebHistory } from "vue-router"
 const routes = 
 [
   {
-    path: '/',
+    path: '/pokemoncardgame/',
     name: 'Main Menu',
     component: () => import('../views/main_menu.vue')
   },
   {
-    path: '/guess_the_mon',
+    path: '/pokemoncardgame/guess_the_mon',
     name: 'Play',
     component: () => import('../views/game.vue')
   },
   {
-    path: '/game_over',
+    path: '/pokemoncardgame/game_over',
     name: 'Game Over',
     component: () => import('../views/game_over.vue')
   }
--- src/views/main_menu.vue
+++ src/views/main_menu.vue
@@ -5,7 +5,7 @@
     </h2>
     
     <button type="button" class="inline-flex items-center px-6 py-3 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-      <router-link to="/guess_the_mon">Start Game</router-link>
+      <router-link to="./guess_the_mon">Start Game</router-link>
     </button>
   </div>
   
